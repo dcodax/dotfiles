5721669682cce5b4a6d5e39b824064a5f416ffc3
Dan Codax <dancodax@gmail.com>
2013-03-28 01:15:28 +0100

    Added update vundle vim functions

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -59,10 +59,33 @@ fi
 
 install_zsh
 
-# Install BundleInstall of vim
 # Download tmux-powerline
 # Download patched-fonts for terminal
 
+#TODO: symlinks in all folders!! No please!!
+
+function vundle-init () {
+  if [ ! -d ~/.vim/bundle/vundle/ ]
+  then
+      mkdir -p ~/.vim/bundle/vundle/
+  fi
+
+  if [ ! -d ~/.vim/bundle/vundle/.git/ ]
+  then
+     git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+     echo "\n\tRead about vim configuration for vundle at https://github.com/gmarik/vundle\n"
+  fi
+}
+
+function vundle () {
+   vundle-init
+   vim -c "execute \"BundleInstall\" | q | q"
+}
 
 
+function vundle-update () {
+	vundle-init
+	vim -c "execute \"BundleInstall!\" | q | q"
+}
 
+vundle
